1b6977bcbc9614545f75bb20ea817d1216576418
jediyuan <jedianakin@163.com>
2016-11-06 15:58:19 +0800

    vue webpack

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+﻿<template>
+  <div>
+    <router-view class="view" transition="test" transition-mode="out-in" keep-alive></router-view>
+  </div>
+</template>
+<script>
+  module.exports={};
+</script>
+<style>
+  .view {
+    position: absolute;
+    height: 100%;
+    min-width: 100%;
+    transition: all .5s ease;
+  }
+  .test-enter, .test-leave {
+    opacity: 0;
+    transform: translate3d(10px, 0, 0);
+  }
+  .v-link-active {
+    color: red;
+  }
+  [v-cloak] {
+    display: none;
+  }
+</style>
